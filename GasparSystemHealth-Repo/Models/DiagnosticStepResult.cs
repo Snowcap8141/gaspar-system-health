48d0747775a301af8ead614ce92241d6a2fcdfc6
moreno <111392918+Snowcap8141@users.noreply.github.com>
2026-04-08 13:48:28 +0200

    Delete GasparSystemHealth-Repo directory

--- GasparSystemHealth-Repo/Models/DiagnosticStepResult.cs
+++
@@ -1,9 +0,0 @@
-namespace GasparSystemHealth.Models;
-
-public sealed class DiagnosticStepResult
-{
-    public required string Title { get; init; }
-    public required string Detail { get; init; }
-    public required string Status { get; init; }
-    public bool Success { get; init; }
-}
